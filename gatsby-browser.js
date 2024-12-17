3d3092852b5bf1bb06b8093a5ab94fc7912e297f
Josh Head <jhead12@users.noreply.github.com>
2024-12-16 16:28:49 -0800

    gatsbyjs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,21 @@ import "@fontsource/dm-sans/700.css"
 import "@fontsource/dm-mono"
 import "@fontsource/dm-mono/500.css"
 
+const facebookPixelId = process.env.GATSBY_FACEBOOK_PIXEL_ID;
+
+
+export const onClientEntry = () => {
+    if (typeof window !== 'undefined') {
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', facebookPixelId);
+      fbq('track', 'PageView');
+    }
+  };
+  
---
+++ gatsby-ssr.js
@@ -0,0 +1,36 @@
+export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
+    const facebookPixelId = process.env.GATSBY_FACEBOOK_PIXEL_ID;
+  
+    setHeadComponents([
+      <script
+        key="meta-pixel-script"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${facebookPixelId}');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />,
+    ]);
+  
+    setPostBodyComponents([
+      <noscript key="meta-pixel-noscript">
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${facebookPixelId}&ev=PageView&noscript=1`}
+          alt="Meta Pixel"
+        />
+      </noscript>,
+    ]);
+  };
+  
