1505638f2464accf20f8582bd2106916947c0262
Josh Head <jhead12@users.noreply.github.com>
2024-12-16 16:00:21 -0800

    onClientEntry

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,10 @@ import "@fontsource/dm-sans/700.css"
 import "@fontsource/dm-mono"
 import "@fontsource/dm-mono/500.css"
 
+export const onClientEntry = () => {
+    if (typeof window !== "undefined" && window.gatsbyPluginGDPRCookiesOptions) {
+      const options = window.gatsbyPluginGDPRCookiesOptions;
+      // Initialize the plugin here
+    }
+  };
+  
